323392ae4cac877a33287576eb2cb43b904aa233
Mike Ebube <mikeebube14@gmail.com>
2025-08-02 09:05:48 +0000

    When we host the site on custom domain the telegram task and face verifi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db, app };
