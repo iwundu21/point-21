fef45d12e777d38e1ae8ea31e44af3b3f359d7f4
Iwundu James <mikeebube14@gmail.com>
2026-01-16 18:00:24 +0000

    Error: [GoogleGenerativeAI Error]: Error fetching from https://generativ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
@@ -8,5 +8,5 @@ export const ai = genkit({
       apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
-  model: 'googleai/gemini-2.0-flash',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
